c4e63eb9b9c5f2ab3ba4a495c1aae03f51772642
arsh-mehta <71285772+arsh-mehta@users.noreply.github.com>
2020-11-10 09:48:14 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -70,7 +70,7 @@ function setup() {
 
 function draw() {
   //trex.debug = true;
-  background("light grey");
+  background(211,211,211);
   text("Score: "+ score, 500,50);
   
   if (gameState===PLAY){
